1697c408579b93df9cedafa89f18cc61b1bc12af
zzq <zhangzq@geninfo.com.cn>
2019-05-08 15:50:07 +0800

    init project

---
+++ formateData.js
@@ -0,0 +1,22 @@
+var e = function(e) {
+    for (
+      var n = new ArrayBuffer(2 * e.length),
+        r = new Uint16Array(n),
+        t = 0,
+        o = e.length;
+      t < o;
+      t++
+    )
+      r[t] = e.charCodeAt(t);
+    return r;
+  },
+  n = function(n) {
+    var r = e(JSON.stringify(n)),
+      t = r.length,
+      o = new ArrayBuffer(4);
+    new DataView(o).setUint32(0, t);
+    var s = new Uint8Array(4 + t);
+    return s.set(new Uint8Array(o), 0), s.set(r, 4), null, s.buffer;
+  };
+
+module.exports = n;
---
+++ handleData.js
@@ -0,0 +1,8 @@
+module.exports=(data)=>{
+    const buf = Buffer.from(data);
+    const tempresult = buf.toString("utf8", 4);
+    const sorted = JSON.parse(tempresult).dojo_list.filter(val =>
+      val.winner_name ? val : null
+    );
+    return sorted
+}
